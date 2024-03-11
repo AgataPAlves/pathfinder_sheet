9a4d98677a92b75dd2003fe37aaf1ca993c9712b
Ágata Alves <111840772+AgataPAlves@users.noreply.github.com>
2024-03-10 23:14:58 -0300

    home

--- pathfinder_sheet/tailwind.config.js
+++ pathfinder_sheet/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
     content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+      extend: {
+          colors: {
+              'Bistre': '#392010',
+              'Flax': '#E7DE95',
+              'Vanilla': '#FBEAB8',
+              'Cornsilk': '#FEFAE0',
+              'Ecru': 'BBA672',
+              'Chamoisee': '9B8059',
+        }
+    },
   },
   plugins: [],
 }
